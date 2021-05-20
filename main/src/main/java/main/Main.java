d4dd2bf861e9cc561961a44451eef880ca37aa1a
client <client@DESKTOP-QPGSR6V>
2021-05-20 22:11:55 +0100

    This is my second to the last phase of Boulder Dash

---
+++ main/src/main/java/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import controller.Controller;
+import model.Model;
+import view.View;
+
+/**
+ * The Class Main.
+ *
+ * 
+ */
+public abstract class Main {
+
+    /**
+     * The main method.
+     *
+     * @param args
+     *            the arguments
+     */
+    public static void main(final String[] args) {
+        final Model model = new Model();
+        model.loadMap(4);
+        final View view = new View(model);
+        final Controller controller = new Controller(view, model);
+        view.setController(controller);
+        controller.run();
+        controller.control();
+
+    }
+}
---
+++ model/src/main/java/model/DAOMap.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+/**
+ * The Class DAOMap.
+ *
+ */
+class DAOMap {
+
+	
+	private Connection connection;
+
+	/**
+	 * Instantiates a new DAO map.
+	 *
+	 * @param connection
+	 *          the connection
+	 * @throws SQLException
+	 *           the SQL exception
+	 */
+	public DAOMap(final Connection connection) throws SQLException {
+		this.connection = connection;
+	}
+
+
+	/**
+	 * Finds the map
+	 * @param id Map ID from DB
+	 * @return Map
+	 */
+	public Map find(final int id) {
+		Map map = new Map();
+
+		try {
+			final String sql = "CALL getMap(?)";
+			final CallableStatement call = this.getConnection().prepareCall(sql);
+			call.setInt(1, id);
+			call.execute();
+			final ResultSet result = call.getResultSet();
+			result.next();
+			map = new Map(result.getObject(1).toString(), (int) result.getObject(2), (int) result.getObject(3), (int) result.getObject(4));
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		}
+		return map;
+	}
+
+	
+	public Map find(final String code) {
+		return null;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	
+}
