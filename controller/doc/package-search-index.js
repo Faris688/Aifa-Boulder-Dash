3003cfc2f9866000b84abdb5159c0bd3c9a96299
client <client@DESKTOP-QPGSR6V>
2021-05-24 00:42:22 +0100

    Javadoc Reports

---
+++ controller/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"contract"},{"l":"controller"},{"l":"entity"},{"l":"main"},{"l":"model"},{"l":"model.type"},{"l":"view"}];updateSearchResults();
